ed64b6e565faa6952a59d852a216c53cc6c32843
ysicing <root@ysicing.net>
2017-10-25 14:30:22 +0800

    [fix] fix version error

--- pkg/subcmd/version.py
+++ pkg/subcmd/version.py
@@ -10,10 +10,11 @@
 
 from . import SubCommand
 
+
 def _show_version(args):
     try:
         from pkg.version import VERSION
-        for name, value in VERSION:
+        for name, value in VERSION.items():
             print(" {0:10s} :\t{1}".format(name, value))
     except ImportError as e:
         print("get version info failed")
