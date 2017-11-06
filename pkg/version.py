a0a3584eb9131b6f1cba49bd24c02fd23baa4d37
ysicing <root@ysicing.net>
2017-11-06 15:46:59 +0800

    2017-11-06 15:46:58 update

--- pkg/version.py
+++ pkg/version.py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python3
-# coding=utf-8
-
-"""
-@version:0.1
-@author: ysicing
-@file: dc-ctl/version.py
-@time: 10/25/17 12:22 AM
-"""
-
 VERSION = {
-    'release':'beta',
-    'version':'v0.1',
-    'gitcommit': 'd955811',
-    'buildTime': '2017-10-24'
+    'release': "beta",
+    'version': "0.1",
+    'gitcommit': "0b67eee",
+    'buildTime': "2017-11-06 15:46:58",
 }
