aa080d9b967a73ac44cd8e4e849b36fc3364675e
ysicing <root@ysicing.net>
2017-11-28 12:14:19 +0800

    [FMT] update version test

--- pkg/version.py
+++ pkg/version.py
@@ -1,6 +1,6 @@
 VERSION = {
     'release': "master",
     'version': "0.1",
-    'gitcommit': "abd63b7",
-    'buildTime': "2017-11-28 12:06:31",
+    'gitcommit': "7d920a5",
+    'buildTime': "2017-11-28 12:14:19",
 }
